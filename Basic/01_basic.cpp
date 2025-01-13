0c012b1647a09072fc32a836691342af5394fc30
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-13 09:28:34 +0530

    data types

---
+++ Basic/01_basic.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello Anirban." << endl;
+	cout << "Bye Ani." << endl;
+	return 0;
+}
---
+++ Basic/02_dataTypes.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+	//int
+	int x = 10;
+	cin >> x;
+	
+	// long
+	long y = 2134;
+	cin >> y;
+	
+	// float
+	cout << x << "\n";
+	cout << y;
+	return 0;
+}
