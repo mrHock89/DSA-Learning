4f60441276d3765ed2b09a3ac55aac3cd8284a67
mrHock89 <147297458+mrHock89@users.noreply.github.com>
2025-01-31 08:06:58 +0530

    patterns learning

--- Basic/Pattern/03_ptn.cpp
+++ Basic/Pattern/03_ptn.cpp
@@ -59,16 +59,38 @@ void print7(int n){
 void print8(int n){
     for(int i = 0; i < n; i++){
         // space
-        for(int j = 0; j < n ; j++){
+        for(int j = 0; j < i ; j++){
         cout << " ";
         }
         // star
-        for(int j = 0; j <2*n - 2 * i + 1; j++){
+        for(int j = 0; j <2*n - (2 * i + 1); j++){
             cout << "*";
         }
         // space
-        for(int j = 0; j < n ; j++){
+        for(int j = 0; j < i ; j++){
         cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print10(int n){
+    for(int i = 1; i <= 2*n -1; i++){
+        int stars = i;
+        if(i > n) stars = 2*n -i;
+        for(int j = 1; j <= stars; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void print11(int n){
+    int start =1;
+    for(int i= 0; i< n; i++){
+        if(i % 2 == 0) start = 1;
+        else start = 0;
+        for(int j = 0; j <= i; j++){
+            cout << start;
+            start = 1- start;
         }
         cout << endl;
     }
@@ -82,6 +104,7 @@ int main(){
         cin >> num;
         // print3(num);
         // print4(num);
-        print7(num);
+        // print7(num);
+        print11(num);
     }
 }
---
+++ Basic/Time and space complexity/Readme.md
@@ -0,0 +1,4 @@
+# Time and space complexity 14th Jan 2025
+- What is time complexity?
+`Rate at which the time taken increases with respect ti the input size`
+- Have to revice some notes
